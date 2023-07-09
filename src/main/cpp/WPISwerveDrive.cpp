b4e255aa741dcc930412074a877b64010fa08673
Jack Jin <79951112+Rvistix@users.noreply.github.com>
2023-07-09 15:50:04 -0400

    The vector variable is in a very sketchy state...
    need swerveModule implementation.

--- src/main/cpp/WPISwerveDrive.cpp
+++ src/main/cpp/WPISwerveDrive.cpp
@@ -1,26 +1,52 @@
 #include "WPISwerveDrive.h"
 
 void WPISwerveDrive::Configure(SwerveConfig &config){
+    SetIdleMode(config.idle_mode);
+    m_ebrake = config.ebrake;
+    m_maxDriveSpeed = config.maxDriveSpeed;
+    m_maxTurnSpeed = config.maxTurnSpeed;
+    m_orientation = config.orientation;
     
-}
-bool WPISwerveDrive::GetEbrake() {
 
+}
+
+
+bool WPISwerveDrive::GetEbrake() {
+    return m_ebrake;
 }
 void WPISwerveDrive::SetEbrake(bool ebrake) {
-
+    m_ebrake = ebrake;
 }
-void WPISwerveDrive::Drive(frc::Translation2d position, double rotation) {
+void WPISwerveDrive::Drive(double x_position, double y_position, double rotation) {
+    
 
+    Drive(x_position * m_maxDriveSpeed, y_position * m_maxDriveSpeed, rotation * m_maxTurnSpeed);
 }
 void WPISwerveDrive::Drive(double vx, double vy, double omega) {
+    
+    Drive(frc::ChassisSpeeds((units::meters_per_second_t)vx, (units::meters_per_second_t)vy, (units::radians_per_second_t)omega));
 
 }
 void WPISwerveDrive::Drive(frc::ChassisSpeeds speed) {
 
+    frc::SwerveDriveKinematics<4> m_kinematics{
+    m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation,
+    m_backRightLocation};
+ 
+
+    auto [fl, fr, bl, br] = m_kinematics.ToSwerveModuleStates(speed);
+    
+    states.push_back(fl);
+    states.push_back(fr);
+    states.push_back(bl);
+    states.push_back(br);
+
+    Drive(states);
+
 }
 void WPISwerveDrive::Drive(std::vector<frc::SwerveModuleState> &state) {
 
-}
+} 
 bool WPISwerveDrive::GetIdleMode() {
 
 }
@@ -28,8 +54,11 @@ void WPISwerveDrive::SetIdleMode(bool idle_mode) {
 
 }
 void WPISwerveDrive::SetRobotOriented() {
-
+    m_orientation = false;
 }
 void WPISwerveDrive::SetFieldOriented() {
-
+    m_orientation = true; 
+}
+bool WPISwerveDrive::GetOrientedMode() {
+    return m_orientation;
 }
--- src/main/include/WPISwerveDrive.h
+++ src/main/include/WPISwerveDrive.h
@@ -1,13 +1,26 @@
 #pragma once
 #include "SwerveDrive.h"
 
+struct SwerveConfig{
+    bool idle_mode;
+    bool ebrake;
+    bool orientation;
+    double maxDriveSpeed;
+    double maxTurnSpeed;
+    //location of motors relative to the centor of the robot
+ 
+
+
+
+};
+
 class WPISwerveDrive : public SwerveDrive
 {
     public:
         virtual void Configure(SwerveConfig &config) = 0;
         virtual bool GetEbrake() override;
         virtual void SetEbrake(bool ebrake) override;
-        virtual void Drive(frc::Translation2d position, double rotation) override;
+        virtual void Drive(double x_position, double y_position, double rotation);
         virtual void Drive(double vx, double vy, double omega) override;
         virtual void Drive(frc::ChassisSpeeds speed) override;
         virtual void Drive(std::vector<frc::SwerveModuleState> &state) override;
@@ -15,10 +28,30 @@ class WPISwerveDrive : public SwerveDrive
         virtual void SetIdleMode(bool idle_mode) override;
         virtual void SetRobotOriented() override;
         virtual void SetFieldOriented() override;
+        virtual bool GetOrientedMode() override; 
+
+
 
     private:
-        bool ebrake;
-        bool orientation;
+
+        frc::Translation2d m_frontLeftLocation{0.381_m, 0.381_m};
+        frc::Translation2d m_frontRightLocation{0.381_m, -0.381_m};
+        frc::Translation2d m_backLeftLocation{-0.381_m, 0.381_m};
+        frc::Translation2d m_backRightLocation{-0.381_m, -0.381_m};
+        frc::SwerveDriveKinematics<4> m_kinematics{
+        m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation,
+        m_backRightLocation};
+
+        std::vector<frc::SwerveModuleState> states;
+        
+        double m_maxDriveSpeed;
+        double m_maxTurnSpeed;
+        bool m_ebrake = false;
+        bool m_driveMotorIdleMode = false;
+        //false is brake and true is coast
+        bool m_orientation = false;
+        //false is robot orientated, true is FieldOrientated. 
+        
 
 
 };
