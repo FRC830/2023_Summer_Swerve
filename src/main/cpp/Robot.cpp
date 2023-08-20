df8cb0274a4c1fb37e58dd5e28356d269d82ba50
Jack Jin <79951112+Rvistix@users.noreply.github.com>
2023-08-20 14:44:40 -0400

    Update Robot.cpp

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -8,28 +8,103 @@
 
 void Robot::SwerveInit(){
   AbsoluteEncoderConfig brAbsConfig;
+  brAbsConfig.encoder = &br_abs_enc;
+  brAbsConfig.is_inverted = BR_ABS_ENC_INVERTED;
+  brAbsConfig.zero_heading = BR_ZERO_HEADING;
   AbsoluteEncoderConfig frAbsConfig;
+  frAbsConfig.encoder = &fr_abs_enc;
+  frAbsConfig.is_inverted = BR_ABS_ENC_INVERTED;
+  frAbsConfig.zero_heading = BR_ZERO_HEADING;
   AbsoluteEncoderConfig blAbsConfig;
   AbsoluteEncoderConfig flAbsConfig;
 
+  AnalogAbsoluteEncoder brAbs;
+  brAbs.Configure(brAbsConfig);
+  AnalogAbsoluteEncoder frAbs;
+  frAbs.Configure(frAbsConfig);
+
+
 
   SwerveTurnMotorConfig brTurnConfig;
+  brTurnConfig.absouluteEncoder=&brAbs;
+  brTurnConfig.d=TURN_D;
+  brTurnConfig.deviceID=BR_TURN_MTR_ID;
+  brTurnConfig.ff=TURN_FF;
+  brTurnConfig.i=TURN_I;
+  brTurnConfig.inverted=BR_TURN_MTR_INVERTED;
+  brTurnConfig.p=TURN_P;
+  brTurnConfig.PID=&br_turn_pid;
+  brTurnConfig.ratio=TURN_GEAR_RATIO;
+  brTurnConfig.relative_Encoder=&br_turn_enc;
+  brTurnConfig.turn_motor=&br_turn_mtr;
   SwerveTurnMotorConfig frTurnConfig;
+  frTurnConfig.absouluteEncoder=&frAbs;
+  frTurnConfig.d=TURN_D;
+  frTurnConfig.deviceID=BR_TURN_MTR_ID;
+  frTurnConfig.ff=TURN_FF;
+  frTurnConfig.i=TURN_I;
+  frTurnConfig.inverted=BR_TURN_MTR_INVERTED;
+  frTurnConfig.p=TURN_P;
+  frTurnConfig.PID=&fr_turn_pid;
+  frTurnConfig.ratio=TURN_GEAR_RATIO;
+  frTurnConfig.relative_Encoder=&fr_turn_enc;
+  frTurnConfig.turn_motor=&fr_turn_mtr;
   SwerveTurnMotorConfig blTurnConfig;
   SwerveTurnMotorConfig flTurnConfig;
 
+  NeoTurnMotor brTurn;
+  brTurn.Configure(brTurnConfig);
+  NeoTurnMotor frTurn;
+  frTurn.Configure(frTurnConfig);
+
+
   SwerveDriveMotorConfig brDriveConfig;
+  brDriveConfig.d = DRIVE_D;
+  brDriveConfig.ff = DRIVE_FF;
+  brDriveConfig.i = DRIVE_I;
+  brDriveConfig.p = DRIVE_P;
+  brDriveConfig.PID = &br_drive_pid;
+  brDriveConfig.ratio = MOTOR_ROT_TO_FT / 60.0;
+  brDriveConfig.encoder = &br_drive_enc;
+  brDriveConfig.motor = &br_drive_mtr;
   SwerveDriveMotorConfig frDriveConfig;
+  frDriveConfig.d = DRIVE_D;
+  frDriveConfig.ff = DRIVE_FF;
+  frDriveConfig.i = DRIVE_I;
+  frDriveConfig.p = DRIVE_P;
+  frDriveConfig.PID = &fr_drive_pid;
+  frDriveConfig.ratio = MOTOR_ROT_TO_FT / 60.0;
+  frDriveConfig.encoder = &fr_drive_enc;
+  frDriveConfig.motor = &fr_drive_mtr;
   SwerveDriveMotorConfig blDriveConfig;
   SwerveDriveMotorConfig flDriveConfig;
 
+  NeoDriveMotor brDrive;
+  brDrive.Configure(brDriveConfig);
+  NeoDriveMotor frDrive;
+  frDrive.Configure(frDriveConfig);
+
   SwerveModuleConfig brModuleConfig;
+  brModuleConfig.driveMotor=&brDrive;
+  brModuleConfig.idleMode=true;
+  brModuleConfig.turnMotor=&brTurn;
   SwerveModuleConfig frModuleConfig;
+  frModuleConfig.driveMotor=&frDrive;
+  frModuleConfig.idleMode=true;
+  frModuleConfig.turnMotor=&frTurn;
   SwerveModuleConfig blModuleConfig;
   SwerveModuleConfig flModuleConfig;
 
   SwerveConfig swerveConfig;
-
+  swerveConfig.backLeftLocation=bl_position;
+  swerveConfig.backRightLocation=br_position;
+  swerveConfig.frontLeftLocation=fl_position;
+  swerveConfig.frontRightLocation=fr_position;
+  swerveConfig.ebrake=true;
+  swerveConfig.idle_mode=true;
+  swerveConfig.maxDriveSpeed=MAX_DRIVE_SPEED_MPS;
+  swerveConfig.maxTurnSpeed=MAX_ANGULAR_VELOCITY_DEGPS;
+  swerveConfig.orientation=IS_ROBOT_ORIENTED_DRIVE;
   m_swerveDrive.Configure(swerveConfig);
 
 
@@ -125,55 +200,55 @@ void Robot::TeleopInit()
   // gyro_config.zero_heading = units::degree_t(90);
   // gyro.Configure(gyro_config);
 
-  AbsoluteEncoderConfig encoderConfig;
-  encoderConfig.encoder = &bl_abs_enc;
-  encoderConfig.is_inverted = BL_ABS_ENC_INVERTED;
-  encoderConfig.zero_heading = BL_ZERO_HEADING;
+  // AbsoluteEncoderConfig encoderConfig;
+  // encoderConfig.encoder = &bl_abs_enc;
+  // encoderConfig.is_inverted = BL_ABS_ENC_INVERTED;
+  // encoderConfig.zero_heading = BL_ZERO_HEADING;
 
-  m_ABSencoder.Configure(encoderConfig);
+  // m_ABSencoder.Configure(encoderConfig);
 
-  m_ABSencoder.SetZeroHeading(m_ABSencoder.GetRawHeading());
+  // m_ABSencoder.SetZeroHeading(m_ABSencoder.GetRawHeading());
   
-  SwerveTurnMotorConfig turnConfig;
-  turnConfig.absouluteEncoder = &m_ABSencoder;
-  turnConfig.d = TURN_D;
-  turnConfig.deviceID = BL_TURN_MTR_ID;
-  turnConfig.ff = TURN_FF;
-  turnConfig.i = TURN_I;
-  turnConfig.inverted = BL_TURN_MTR_INVERTED;
-  turnConfig.p = TURN_P;
-  turnConfig.PID = &bl_turn_pid;
-  turnConfig.ratio = MOTOR_ROT_TO_DEG;
-  turnConfig.relative_Encoder = &bl_turn_enc;
-  turnConfig.turn_motor = &bl_turn_mtr;
+  // SwerveTurnMotorConfig turnConfig;
+  // turnConfig.absouluteEncoder = &m_ABSencoder;
+  // turnConfig.d = TURN_D;
+  // turnConfig.deviceID = BL_TURN_MTR_ID;
+  // turnConfig.ff = TURN_FF;
+  // turnConfig.i = TURN_I;
+  // turnConfig.inverted = BL_TURN_MTR_INVERTED;
+  // turnConfig.p = TURN_P;
+  // turnConfig.PID = &bl_turn_pid;
+  // turnConfig.ratio = MOTOR_ROT_TO_DEG;
+  // turnConfig.relative_Encoder = &bl_turn_enc;
+  // turnConfig.turn_motor = &bl_turn_mtr;
 
-  m_turnMotor.Configure(turnConfig);
+  // m_turnMotor.Configure(turnConfig);
 
-  SwerveDriveMotorConfig driveConfig;
-  driveConfig.d = DRIVE_D;
-  driveConfig.ff = DRIVE_FF;
-  driveConfig.i = DRIVE_I;
-  driveConfig.p = DRIVE_P;
-  driveConfig.PID = &bl_drive_pid;
-  driveConfig.ratio = MOTOR_ROT_TO_FT / 60.0;
-  driveConfig.encoder = &bl_drive_enc;
-  driveConfig.motor = &bl_drive_mtr;
+  // SwerveDriveMotorConfig driveConfig;
+  // driveConfig.d = DRIVE_D;
+  // driveConfig.ff = DRIVE_FF;
+  // driveConfig.i = DRIVE_I;
+  // driveConfig.p = DRIVE_P;
+  // driveConfig.PID = &bl_drive_pid;
+  // driveConfig.ratio = MOTOR_ROT_TO_FT / 60.0;
+  // driveConfig.encoder = &bl_drive_enc;
+  // driveConfig.motor = &bl_drive_mtr;
 
-  m_driveMotor.Configure(driveConfig);
+  // m_driveMotor.Configure(driveConfig);
 
   
-  SwerveModuleConfig moduleConfig;
-  moduleConfig.driveMotor = &m_driveMotor;
-  moduleConfig.idleMode = true;
-  moduleConfig.turnMotor = &m_turnMotor;
+  // SwerveModuleConfig moduleConfig;
+  // moduleConfig.driveMotor = &m_driveMotor;
+  // moduleConfig.idleMode = true;
+  // moduleConfig.turnMotor = &m_turnMotor;
 
 
-  m_swerveModule_BL.Configure(moduleConfig);
+  // m_swerveModule_BL.Configure(moduleConfig);
 
 
 
-  frc::SmartDashboard::PutNumber("Speed", 0.0);
-  frc::SmartDashboard::PutNumber("ang", 0.0);
+  // frc::SmartDashboard::PutNumber("Speed", 0.0);
+  // frc::SmartDashboard::PutNumber("ang", 0.0);
   
   
 
@@ -208,7 +283,7 @@ void Robot::TeleopPeriodic()
 
   frc::SwerveModuleState state{units::feet_per_second_t(speed), units::degree_t(angle)};
   //bl_turn_pid.SetReference(angle, rev::CANSparkMax::ControlType::kPosition);
-  m_swerveModule_BL.SetState(state);
+  //m_swerveModule_BL.SetState(state);
 
   
   // frc::SmartDashboard::PutNumber("Set Position", sp);
