39aee9a6e2d2c41bfdbb4d91c13c21d6c1d0a10c
TheCrudeOilBro <lucasyuan1981@gmail.com>
2024-01-19 18:09:24 -0500

    Increase max turning speed.

--- src/main/cpp/WPISwerveDrive.cpp
+++ src/main/cpp/WPISwerveDrive.cpp
@@ -47,9 +47,12 @@ void WPISwerveDrive::Drive(double x_position, double y_position, double rotation
      (units::feet_per_second_t)x_position * m_maxDriveSpeed, 
      (units::feet_per_second_t)y_position * m_maxDriveSpeed, 
      (units::degrees_per_second_t)rotation * m_maxTurnSpeed);
+
 }
 
 void WPISwerveDrive::Drive(units::feet_per_second_t vx, units::feet_per_second_t vy, units::degrees_per_second_t omega) {
+    frc::SmartDashboard::PutNumber("Omega", static_cast<double>(omega));
+
     if (!m_orientation)
     {
         Drive(frc::ChassisSpeeds{vx, vy, omega});   
